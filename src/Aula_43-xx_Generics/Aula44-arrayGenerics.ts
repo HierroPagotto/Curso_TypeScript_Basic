a41d762ad5b0800526785846c61071d4093d9acb
HierroPagotto <132371485+HierroPagotto@users.noreply.github.com>
2024-10-10 17:54:44 -0300

    update

---
+++ src/Aula_43-xx_Generics/Aula44-arrayGenerics.ts
@@ -0,0 +1,17 @@
+const arrayNumeros: Array<number> = [1, 2, 3, 4, 5, 6];
+console.log(arrayNumeros);
+
+async function promiseAsync(){
+  return 1;
+}
+
+function minhaPromise(): Promise<number>{
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 1000);
+  });
+}
+
+promiseAsync().then((resultado)=> console.log(resultado + 1));
+minhaPromise().then((resultado)=> console.log(resultado + 1));
