808fe76883700a7dde93e22bd327a27ff371294f
HierroPagotto <132371485+HierroPagotto@users.noreply.github.com>
2024-10-14 18:02:30 -0300

    update

--- src/typescriptsolid/src/Legacy/shopping-cart-legacy.ts
+++ src/typescriptsolid/src/Legacy/shopping-cart-legacy.ts
@@ -1,7 +1,7 @@
 type CartItem = { name:string; price: number};
 type OrderStatus = 'open' | 'closed'
 
-export class ShoppingCart{
+export class ShoppingCartLegacy{
   private readonly _items: CartItem[] = [];
   private _orderStatus: OrderStatus = 'open';
 
@@ -57,7 +57,7 @@ export class ShoppingCart{
   }
 }
 
-const shoppingCart = new ShoppingCart();
+const shoppingCart = new ShoppingCartLegacy();
 shoppingCart.addItem({name: 'Camiseta', price: 49.9});
 shoppingCart.addItem({name: 'Caderno', price: 9.9});
 shoppingCart.addItem({name: 'Lápis', price: 1.59});
