3d1b8e06d405875cbf7c6d241f9c0eeb1e643a8d
HierroPagotto <132371485+HierroPagotto@users.noreply.github.com>
2024-10-15 11:01:24 -0300

    update

--- src/typescriptsolid/src/srp/main.ts
+++ src/typescriptsolid/src/srp/main.ts
@@ -1,18 +1,23 @@
-import { ShoppingCart } from "./shopping-cart";
-import { Order } from "./order";
-import { Persistency } from "./persistency";
-import { Messaging } from './messaging';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Order } from './classes/order';
+import { Messaging } from './services/messaging';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { FiftyPercentDiscount } from './classes/discount';
 
-const shoppingCart = new ShoppingCart();
+const fiftyPercentDiscount = new FiftyPercentDiscount();
+const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
 const order = new Order(shoppingCart, messaging, persistency);
-shoppingCart.addItem({name: 'Camiseta', price: 49.9});
-shoppingCart.addItem({name: 'Caderno', price: 9.9});
-shoppingCart.addItem({name: 'Lápis', price: 1.59});
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
+shoppingCart.addItem(new Product('Lápis', 1.59));
 
 console.log(shoppingCart.items);
 console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDicount());
 console.log(order.orderStatus);
 order.checkout();
 console.log(order.orderStatus);
