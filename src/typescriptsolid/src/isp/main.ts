88b5fede908ecf6f877792f6cf48ff137f7adfe1
HierroPagotto <132371485+HierroPagotto@users.noreply.github.com>
2024-10-15 15:10:18 -0300

    update

--- src/typescriptsolid/src/isp/main.ts
+++ src/typescriptsolid/src/isp/main.ts
@@ -4,15 +4,20 @@ import { Messaging } from './services/messaging';
 import { Persistency } from './services/persistency';
 import { Product } from './classes/product';
 import { FiftyPercentDiscount } from './classes/discount';
+import { IndividualCustomer, EnterpriseCustomer} from './classes/customer';
 
 const fiftyPercentDiscount = new FiftyPercentDiscount();
 const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
-const order = new Order(shoppingCart, messaging, persistency);
+//const individualcustomer = new IndividualCustomer('Hierro', ' Pagotto', '000.111.222-33','');
 
-shoppingCart.addItem(new Product('Camiseta', 49.91));
-shoppingCart.addItem(new Product('Caderno', 9.9123));
+const enterpriseCustomer = new EnterpriseCustomer('Company',
+  '444.555/0001-66');
+const order = new Order(shoppingCart, messaging, persistency, enterpriseCustomer);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
 shoppingCart.addItem(new Product('Lápis', 1.59));
 
 console.log(shoppingCart.items);
