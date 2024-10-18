cb592077c0366e5cc4dbf842a3f35c48b56c3346
HierroPagotto <132371485+HierroPagotto@users.noreply.github.com>
2024-10-18 15:36:14 -0300

    update jest teste

--- src/typescriptJest/classes/customer.spec.ts
+++ src/typescriptJest/classes/customer.spec.ts
@@ -1,43 +1,47 @@
-import { IndividualCustomer, EnterpriseCustomer } from "./customer";
+import { IndividualCustomer, EnterpriseCustomer } from './customer';
 
-const creatIndividualCustomer = (firstName: string,
+const createIndividualCustomer = (
+  firstName: string,
   lastName: string,
-  cpf: string
+  cpf: string,
 ): IndividualCustomer => {
   return new IndividualCustomer(firstName, lastName, cpf);
-}
-
-const creatEnterpriseCustomer = (Name: string,
-  cnpj: string
-): EnterpriseCustomer => {
-  return new EnterpriseCustomer(Name, cnpj);
 };
+
+const createEnterpriseCustomer = (
+  name: string,
+  cnpj: string,
+): EnterpriseCustomer => {
+  return new EnterpriseCustomer(name, cnpj);
+};
+
+afterEach(() => jest.clearAllMocks());
 
 describe('IndividualCustomer', () => {
   it('should have firstName, lastName and cpf', () => {
-    const sut = creatIndividualCustomer('Hierro', 'Pagotto', '123.456.789-00');
-    expect(sut).toHaveProperty('firstName', 'Hierro'),
-    expect(sut).toHaveProperty('lastName', 'Pagotto'),
-    expect(sut).toHaveProperty('cpf', '123.456.789-00');
+    const sut = createIndividualCustomer('Hierro', 'Pagotto', '111.111');
+    expect(sut).toHaveProperty('firstName', 'Hierro');
+    expect(sut).toHaveProperty('lastName', 'Pagotto');
+    expect(sut).toHaveProperty('cpf', '111.111');
   });
 
-  it('should have methods to get name and idn', () => {
-    const sut = creatIndividualCustomer('Hierro', 'Pagotto', '123.456.789-00');
-    expect(sut.getName).toBe('Hierro Pagotto');
-    expect(sut.getIDN).toBe('123.456.789-00');
+  it('should have methods to get name and idn for individual customers', () => {
+    const sut = createIndividualCustomer('Hierro', 'Pagotto', '111.111');
+    expect(sut.getName()).toBe('Hierro Pagotto');
+    expect(sut.getIDN()).toBe('111.111');
   });
 });
 
 describe('EnterpriseCustomer', () => {
-  it('should have firstName, lastName and cpf', () => {
-    const sut = creatEnterpriseCustomer('Udemy', '12.345.678/0001-90');
-    expect(sut).toHaveProperty('Name', 'Udemy'),
-    expect(sut).toHaveProperty('cnpj', '12.345.678/0001-90');
+  it('should have name and cnpj', () => {
+    const sut = createEnterpriseCustomer('Udemy', '222');
+    expect(sut).toHaveProperty('name', 'Udemy');
+    expect(sut).toHaveProperty('cnpj', '222');
   });
 
-  it('should have methods to get name and idn', () => {
-    const sut = creatEnterpriseCustomer('Udemy', '12.345.678/0001-90');
-    expect(sut).toBe('Udemy'),
-    expect(sut).toBe('12.345.678/0001-90');
+  it('should have methods to get name and idn for enterprise customers', () => {
+    const sut = createEnterpriseCustomer('Udemy', '222');
+    expect(sut.getName()).toBe('Udemy');
+    expect(sut.getIDN()).toBe('222');
   });
 });
