27e3ff2f5bf0f407f989f37c7eec9d133d0e16d7
HierroPagotto <132371485+HierroPagotto@users.noreply.github.com>
2024-10-23 11:32:34 -0300

    update and add buuilder

---
+++ src/typescriptGOF/creational/builder/index.ts
@@ -0,0 +1,17 @@
+import { MainDishBuilder } from "./classes/main-dish-builder";
+import { VeganDishBuilder } from "./classes/vegan-dish-builder";
+
+const mainDishBuilder =  new MainDishBuilder();
+mainDishBuilder.makeMeal().makeDessert();
+console.log(mainDishBuilder.getMeal());
+console.log(mainDishBuilder.getPrice());
+
+mainDishBuilder.reset();
+const meal2 = mainDishBuilder.makeBeverage().getMeal();
+console.log(meal2);
+
+const veganDishBuilder = new VeganDishBuilder();
+const veganMeal = veganDishBuilder.makeMeal().getMeal();
+console.log(veganMeal);
+
+
